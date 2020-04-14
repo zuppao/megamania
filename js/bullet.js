c4a06a202e4939797dc292b5465c9fcd3b8c30f9
Alexandre Tadeu Zuppo <alexandre.zuppo@grupovaliant.com.br>
2020-04-13 21:08:32 -0300

    some bug fixes... Prototype somethings...

--- js/bullet.js
+++ js/bullet.js
@@ -4,7 +4,7 @@ class ShipBullet{
         this.y = _y;    
     }
     
-    Show = function () {
+    /*Show = function () {
         stroke(176,60,60);
         strokeWeight(3);
         line(this.x, this.y, this.x, this.y+18);
@@ -21,7 +21,27 @@ class ShipBullet{
         } else {
             return false;
         }
-    }
+    }*/
+}
+
+
+ShipBullet.prototype.Show = function () {
+        stroke(176,60,60);
+        strokeWeight(3);
+        line(this.x, this.y, this.x, this.y+18);
+}
+
+ShipBullet.prototype.Move = function () {
+        this.y -= 10;
+}
+
+ShipBullet.prototype.Hits = function (_enemy) {
+        if (this.y >= _enemy.y && this.y < (_enemy.y + _enemy.height) &&
+            this.x >= _enemy.x && this.x < (_enemy.x + _enemy.width)) {
+            return true;
+        } else {
+            return false;
+        }
 }
 
 
@@ -31,7 +51,7 @@ class EnemyBullet{
         this.y = _y;    
     }
 
-    Show = function () {
+    /*Show = function () {
         stroke(42,48,132);
         strokeWeight(3);
         line(this.x, this.y, this.x, this.y+12);
@@ -42,13 +62,6 @@ class EnemyBullet{
     }
 
     Hits = function(_ship) {
-        //if(this.x>_ship.x && this.x<(_ship.x+_ship.width) &&
-        //    (this.y+18) >= _ship.y && this.y<= (_ship.y+_ship.height)) {
-        //    return true;
-        //} else {
-        //    return false;
-        //}
-
         if(this.x>=_ship.collider.x1 && this.x<=_ship.collider.x2 &&
             (this.y+18) >= _ship.collider.y1 && this.y<= _ship.collider.y2) {
             return true;
@@ -56,6 +69,26 @@ class EnemyBullet{
             return false;
         }
 
-    }
+    }*/
+
+}
+
+EnemyBullet.prototype.Show = function () {
+        stroke(42,48,132);
+        strokeWeight(3);
+        line(this.x, this.y, this.x, this.y+12);
+}
+
+EnemyBullet.prototype.Move = function () {
+        this.y += 5;
+}
+
+EnemyBullet.prototype.Hits = function(_ship) {
+        if(this.x>=_ship.collider.x1 && this.x<=_ship.collider.x2 &&
+            (this.y+18) >= _ship.collider.y1 && this.y<= _ship.collider.y2) {
+            return true;
+        } else {
+            return false;
+        }
 
 }
--- js/enHamburger.js
+++ js/enHamburger.js
@@ -2,13 +2,13 @@ class Hamburger {
 //function Hamburger(_x,_y) {
 	constructor(_x,_y,_speed) {
 		this.speed = _speed;
-        var passedTime, downStep, downTimes;
+        var passedTime;//, downStep, downTimes;
     	this.x = _x;
     	this.y = _y;
-    	this.width = 30;
-    	this.height = 16;
+    	//this.width = 30;
+    	//this.height = 16;
     	this.passedTime=0;
-        this.downStep=8;this.downTimes=0;
+        //this.downStep=8;this.downTimes=0;
 
     	this.enemyColor = color(192,112,176);
     	this.windowPos = {
@@ -21,7 +21,7 @@ class Hamburger {
     //}
 	}
 
-    Show = function () {
+    /*Show = function () {
         this.Update();
     	//fill(255, 0, 200);
         //rect(this.x, this.y, this.width, this.height);    
@@ -55,9 +55,6 @@ class Hamburger {
                 this.downStep*=-1;
             }
         }
-
-
-
     }
 
     Move = function () {
@@ -70,6 +67,62 @@ class Hamburger {
   			x : this.x,
   			y : this.y+6
   		}
-    }
+    }*/
 
+}
+
+
+
+
+Hamburger.prototype.downStep=8;
+Hamburger.prototype.downTimes=0;
+
+Hamburger.prototype.width = 30;
+Hamburger.prototype.height = 16;
+Hamburger.prototype.Show = function () {
+        this.Update();
+        //fill(255, 0, 200);
+        //rect(this.x, this.y, this.width, this.height);    
+        noStroke();
+        
+
+        //---------------------------
+        fill(this.enemyColor);
+        rect(this.x+7, this.y,this.width/2,this.height);
+        rect(this.x, this.y+2,this.width,this.height-4);
+
+        fill(0);
+        rect(this.x-1, this.y+4,this.width+2,this.height-8);
+
+        fill(this.enemyColor);
+        rect(this.windowPos.x, this.windowPos.y,8,this.height-12);
+        rect(this.windowPos.x+3+8, this.windowPos.y,8,this.height-12);
+        rect(this.windowPos.x+6+16, this.windowPos.y,8,this.height-12);
+        //---------------------------
+}
+
+Hamburger.prototype.Update = function (){
+        this.passedTime+=deltaTime;
+
+        if(this.passedTime>=5000){
+            this.passedTime=0;
+            this.y+=this.downStep;
+            this.downTimes++;
+            if(this.downTimes===3){
+                this.downTimes=0;
+                this.downStep*=-1;
+            }
+        }
+}
+
+Hamburger.prototype.Move = function () {
+        if(this.x<= -this.width){
+            this.x = width;
+        }
+
+        this.x-=2*this.speed;
+        this.windowPos = {
+            x : this.x,
+            y : this.y+6
+        }
 }
